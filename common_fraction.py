a5517877119cef02c8cf5a654b4529211b2980f8
hikaruvi <daniilrybkin32@gmail.com>
2026-02-05 21:00:55 +0700

    feat: add sign definition

--- common_fraction.py
+++ common_fraction.py
@@ -1,17 +1,41 @@
 from enum import Enum
 
+
 class Sign(Enum):
     PLUS = 0
     MINUS = 1
 
+
 class CommonFraction:
-    def __init__(self, top = 1, bottom = 1, sign = Sign.PLUS):
+    def _change_sign(self):
+        if self._sign == Sign.PLUS:
+            self._sign = Sign.MINUS
+        else:
+            self._sign = Sign.PLUS
+
+    def _normalize_sign(self):
+        sign_counter = 0
+
+        if self._top < 0:
+            sign_counter += 1
+        if self._bottom < 0:
+            sign_counter += 1
+
+        if sign_counter % 2:
+            self._change_sign()
+            sign_counter = 0
+
+        self._top = abs(self._top)
+        self._bottom = abs(self._bottom)
+
+    def __init__(self, _top=1, _bottom=1, sign=Sign.PLUS):
+        # числитель и знаменатель
+        self._top = _top
+        self._bottom = _bottom
+
         # знак числа
-        
-
-        self.top = abs(top)
-        self.bottom = abs(bottom)
-
+        self._sign = sign
+        self._normalize_sign()
 
     @staticmethod
     def gcd(in_a, in_b):
@@ -26,7 +50,35 @@ class CommonFraction:
         return a
 
     def reduce(self):
-        cmn_divider = self.gcd(self.top, self.bottom)
-        self.top /= cmn_divider
-        self.bottom /= cmn_divider
-        
+        cmn_divider = self.gcd(self._top, self._bottom)
+        self._top /= cmn_divider
+        self._bottom /= cmn_divider
+
+    @property
+    def top(self):
+        return self._top
+
+    @top.setter
+    def top(self, value):
+        if value < 0:
+            self._change_sign()
+        self._top = value
+
+    @property
+    def bottom(self):
+        return self._bottom
+
+    @bottom.setter
+    def bottom(self, value):
+        if value < 0:
+            self._change_sign()
+        self._bottom = value
+
+    @property
+    def sign(self):
+        return self._sign
+
+    @sign.setter
+    def sign(self, value):
+        if value == Sign.MINUS:
+            self._change_sign()
--- test_common_fraction.py
+++ test_common_fraction.py
@@ -1,9 +1,12 @@
 import pytest
 from common_fraction import CommonFraction
+from common_fraction import Sign
+
 
 @pytest.fixture
 def cf():
     return CommonFraction()
+
 
 def test_setup_cf(cf):
     cf.top = 10
@@ -11,37 +14,77 @@ def test_setup_cf(cf):
 
     assert cf.top == 10 and cf.bottom == 3
 
-def add(a, b):
-    return a + b
 
-@pytest.mark.parametrize("a,b,expected", [
-    (1, 1, 1),
-    (2, 4, 2),
-    (12, 8, 4),
-    (54, 24, 6),
-    (13, 13, 13),
-    (0, 10, 10),
-    (10, 0, 10),
-    (-10, 15, 5),
-    (10, -15, 5),
-    (-10, -15, 5),
-    (1071, 462, 21), 
-])
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (1, 1, 1),
+        (2, 4, 2),
+        (12, 8, 4),
+        (54, 24, 6),
+        (13, 13, 13),
+        (0, 10, 10),
+        (10, 0, 10),
+        (-10, 15, 5),
+        (10, -15, 5),
+        (-10, -15, 5),
+        (1071, 462, 21),
+    ],
+)
 def test_gcd(a, b, expected, cf):
-    assert cf.gcd(a,b) == expected
-    assert cf.gcd(b,a) == expected
+    assert cf.gcd(a, b) == expected
+    assert cf.gcd(b, a) == expected
 
 
-@pytest.mark.parametrize("top, bottom, top_exp, bottom_exp", [
-    # basic
-    (1, 1, 1, 1),
-    (2, 2, 1, 1),
-
-    # negative
-])
+@pytest.mark.parametrize(
+    "top, bottom, top_exp, bottom_exp",
+    [
+        # basic
+        (1, 1, 1, 1),
+        (2, 2, 1, 1),
+        # negative
+    ],
+)
 def test_reduce(top, bottom, top_exp, bottom_exp, cf):
     cf.top = top
     cf.bottom = bottom
     cf.reduce()
 
     assert cf.top == top_exp and cf.bottom == bottom_exp
+
+
+@pytest.mark.parametrize(
+    "top, bottom, sign, exp_sign",
+    [
+        pytest.param(5, 5, Sign.PLUS, Sign.PLUS),
+        pytest.param(5, -5, Sign.PLUS, Sign.MINUS),
+        pytest.param(-5, 5, Sign.PLUS, Sign.MINUS),
+        pytest.param(-5, 5, Sign.MINUS, Sign.PLUS),
+        pytest.param(5, -5, Sign.MINUS, Sign.PLUS),
+        pytest.param(-5, -5, Sign.PLUS, Sign.PLUS),
+        pytest.param(-5, -5, Sign.MINUS, Sign.MINUS),
+    ],
+)
+def test_cf_on_init(top, bottom, sign, exp_sign):
+    cf = CommonFraction(top, bottom, sign)
+    assert cf.sign == exp_sign
+
+
+@pytest.mark.parametrize(
+    "top, bottom, sign, exp_sign",
+    [
+        pytest.param(5, 5, Sign.PLUS, Sign.PLUS),
+        pytest.param(5, -5, Sign.PLUS, Sign.MINUS),
+        pytest.param(-5, 5, Sign.PLUS, Sign.MINUS),
+        pytest.param(-5, 5, Sign.MINUS, Sign.PLUS),
+        pytest.param(5, -5, Sign.MINUS, Sign.PLUS),
+        pytest.param(-5, -5, Sign.PLUS, Sign.PLUS),
+        pytest.param(-5, -5, Sign.MINUS, Sign.MINUS),
+    ],
+)
+def test_cf_on_set(top, bottom, sign, exp_sign, cf):
+    cf.top = top
+    cf.bottom = bottom
+    cf.sign = sign
+
+    assert cf.sign == exp_sign
