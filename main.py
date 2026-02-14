cda8d072f6a016b6b4a6dd3268f62f8541569521
hikaruvi <daniilrybkin32@gmail.com>
2026-02-14 14:18:45 +0700

    fix: add new main file

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ def main() -> None:
 
     dir_for_answers = "answer"
     answer_filepath = f"./{dir_for_answers}/answer.json"
-    matrices_filepath = f"./{dir_for_answers}/matrix.txt"
 
     os.makedirs(dir_for_answers, exist_ok=True)
-    create_or_truncate_file(matrices_filepath)
     create_or_truncate_file(answer_filepath)
 
     answer_obj, intermediate_matrices = solve_linear_system(matrix)
 
-    write_answer_to_file(answer_filepath, answer_obj)
+    full_answer = {}
     for matrix in intermediate_matrices:
-        write_intermediate_matrix_to_file(matrices_filepath, matrix)
+        key, value = convert_matrix_to_json_field(matrix)
+        full_answer[key] = value
+    full_answer["solution"] = answer_obj
+
+    write_answer_to_file(answer_filepath, full_answer)
 
 
 if __name__ == "__main__":
